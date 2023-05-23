f220e8fe32890815431851d8dacbaf67ab2e874a
Nikolas <timofeevnik41@gmail.com>
2023-05-23 15:11:04 +0400

    fix: cors

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth_router
 
 
 app = FastAPI(title='Travelator REST API', version='0.0.1amogus')
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "PATCH", "DELETE", "PUT"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
