a3e882a25ec65bf5ee4abec4583a43d7faa13910
Berlimor <eldaryusufovooo@gmail.com>
2023-05-23 18:31:07 +0300

    Added find method

--- services/city.py
+++ services/city.py
@@ -1,27 +1,20 @@
 from pydantic import BaseModel
 from .database import _MongoClient
 
-class City:
+class City(BaseModel):
     id: str
-    ya_id: str
     name: str
-    country: str
-    description: str
-    coordinates: tuple(float, float)
-    rating: int
 
 
 class City_Service(_MongoClient):
     def __init__(self) -> None:
         super().__init__()
         self.cities_collection = self.db['cities']
-        # self.events = self.db.cities.events # События 
-        # self.excursions = self.db.excursions # Экскурсии
-        # self.hotels = self.db.hotels # Отели
-        # self.places = self.db.places # Достопримечательности 
-        # self.restaurants = self.db.restaurants # Рестораны
 
     async def _exists(self, name: str) -> bool:
         return bool(await self.cities_collection.findOne({'title': name}))
 
+    async def find_city_by_name(self, name) -> City:
+        return await self.cities_collection.find({'title': name}, {'title': 1})
+    
     
